ed1800ce324c27d1bf52eaf283f588265f5f4f9d
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2022-10-11 10:22:58 -0300

    Updates flatpak-setup.sh

--- flatpak-setup.sh
+++ flatpak-setup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-flatpak upgrade
+flatpak upgrade -y
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo -y
 flatpak install flathub im.riot.Riot -y
 flatpak install flathub org.videolan.VLC -y
+flatpak install flathub io.gitlab.librewolf-community -y
+flatpak install flathub com.github.tchx84.Flatseal -y
