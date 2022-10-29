03c5bc8ab115a3cb2dfb8359bece930f52a9ca86
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2022-10-29 17:09:09 -0300

    Update flatpak script

--- flatpak.sh
+++ flatpak.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --if-not-exists kdeapps --from https://distribute.kde.org/kdeapps.flatpakrepo
+
+flatpak update --appstream
 flatpak upgrade -y
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo -y
+
 flatpak install flathub im.riot.Riot -y
-flatpak install flathub org.videolan.VLC -y
-flatpak install flathub io.gitlab.librewolf-community -y
+
+# flatpak install flathub org.videolan.VLC -y
+flatpak install flathub io.mpv.Mpv -y
+
+# flatpak install flathub io.gitlab.librewolf-community -y
+
 flatpak install flathub com.github.tchx84.Flatseal -y
