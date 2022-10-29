bb43578eea15593e2f76df480eb82930386a139c
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2022-10-29 17:08:50 -0300

    Updates
    Enable automatic updates
    
    Install RPM Fusion repos and two packages

--- setup.sh
+++ setup.sh
@@ -4,9 +4,22 @@ sudo tee -a /etc/sudoers <<EOT
 Defaults !tty_tickets
 EOT
 
+# Enable automatic updates
+sudo sed -i 's/#AutomaticUpdatePolicy=none/AutomaticUpdatePolicy=stage/' /etc/rpm-ostreed.conf
+rpm-ostree reload
+systemctl enable rpm-ostreed-automatic.timer --now
+
 # rpm-ostree upgrade
 # rpm-ostree override remove plasma-discover plasma-discover-notifier plasma-discover-flatpak
 # rpm-ostree kargs --append="pci=nocrs" # Fix for my Lenovo Ideapad 3 14IIL05
+
+# RPM Fusion repos
+# I install ffmpeg from here
+rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm --apply-live
+# I install intel-media-driver from here
+rpm-ostree install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm --apply-live
+
+rpm-ostree install intel-media-driver ffmpeg --apply-live
 
 # Change the hostname
 echo "New hostname name, the default value being 'fedora':"
