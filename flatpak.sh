303aa5259c3b5714b1878c19b96f0e756732a9c1
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2022-11-04 13:32:32 -0300

    Use --user by default

--- flatpak.sh
+++ flatpak.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak remote-add --if-not-exists kdeapps --from https://distribute.kde.org/kdeapps.flatpakrepo
+flatpak --user remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak --user remote-add --if-not-exists kdeapps --from https://distribute.kde.org/kdeapps.flatpakrepo
 
-flatpak update --appstream
-flatpak upgrade -y
-
-flatpak install flathub im.riot.Riot -y
-
-# flatpak install flathub org.videolan.VLC -y
-flatpak install flathub io.mpv.Mpv -y
+flatpak --user update --appstream
+flatpak --user -y upgrade
 
 # flatpak install flathub io.gitlab.librewolf-community -y
-
-flatpak install flathub com.github.tchx84.Flatseal -y
+# flatpak install flathub org.videolan.VLC -y
+flatpak --user -y install flathub \
+	org.qbittorrent.qBittorrent \
+	im.riot.Riot \
+	io.mpv.Mpv \
+	com.github.tchx84.Flatseal
